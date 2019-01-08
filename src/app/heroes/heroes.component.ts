448e0fef448ac7fb4a34bfb07820e8d4eebe7e79
Christos Papaloukas <chris.papaloukas@gmail.com>
2019-01-08 20:09:27 +0200

    2nd day commit

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { Hero } from '../classes/classes'
 
 @Component({
-  selector: 'app-heroes', // component name
-  templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+    selector: 'app-heroes', // component name
+    templateUrl: './heroes.component.html',
+    styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit() {
-  }
+    heroname = 'Deadpool';
+    newhero = 'Spiderman';
+    hero: Hero = new Hero();
+
+    ngOnInit() {
+        // this.hero = `The new hero is: ${this.newhero}`;
+        let temp_var = 5 // let: is demanded to have local variable
+        this.heroname = 'Batman';
+        this.hero.id = 11
+        this.hero.name = 'Heroku'
+    }
+
+    doIt(){
+        alert(`Hey, this is ${this.hero.name}!`)
+    }
 
 }

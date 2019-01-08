56d85bcdd20aaaa0ce157a01511197eebda818d4
Christos Papaloukas <chris.papaloukas@gmail.com>
2019-01-08 16:27:22 +0200

    Initial commit

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes', // component name
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

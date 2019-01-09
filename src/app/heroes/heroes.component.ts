672da0a1c631c8cd2f60905615832e4809efe0b9
Christos Papaloukas <chris.papaloukas@gmail.com>
2019-01-09 20:06:55 +0200

    3rd day commit

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Hero } from '../classes/classes'
+import { Hero } from '../classes/hero'
+import { HEROES } from '../utils/mock-heroes';
 
 @Component({
     selector: 'app-heroes', // component name
@@ -12,18 +13,23 @@ export class HeroesComponent implements OnInit {
 
     heroname = 'Deadpool';
     newhero = 'Spiderman';
-    hero: Hero = new Hero();
+    // hero: Hero = new Hero();
+    heroes = HEROES;
+    selectedHero: Hero;
 
     ngOnInit() {
         // this.hero = `The new hero is: ${this.newhero}`;
-        let temp_var = 5 // let: is demanded to have local variable
-        this.heroname = 'Batman';
-        this.hero.id = 11
-        this.hero.name = 'Heroku'
+        // let temp_var = 5 // let: is demanded to have local variable
+        // this.heroname = 'Batman';
+        // this.hero.id = 11
+        // this.hero.name = 'Heroku'
     }
 
-    doIt(){
-        alert(`Hey, this is ${this.hero.name}!`)
+    doIt() {
+        // alert(`Hey, this is ${this.hero.name}!`)
     }
 
+    onSelect(hero: Hero): void {
+        this.selectedHero = hero;
+    }
 }
